9e7d66b73c8f30dfd206176f356985d38ebb3f61
William Blair <60581672+williamblair333@users.noreply.github.com>
2022-02-06 12:34:33 -0500

    Add files via upload

--- Kronos_Export_457b.ps1
+++ Kronos_Export_457b.ps1
@@ -1,9 +1,10 @@
 <# 
-File:       kronos-nationwide-import-prep.ps1
+File:       kronos_export_457b.ps1
 Date:       2021APR15
 Author:     William Blair
 Contact:    williamblair333@gmail.com
 Note:       Runs from any folder but folder names with spaces will error if double-clicking to run
+
 
 #>
 
@@ -31,9 +32,9 @@ Clean up user defined variables http://blog.insidemicrosoft.com/2017/05/28/how-t
 
 # Delete leftover xlsm and csv files
 Add-Type -AssemblyName PresentationFramework 
-[System.Windows.MessageBox]::Show("Warning! Excel is going to close!  Please save all work before you click OK.")
+#[System.Windows.MessageBox]::Show("Warning! Excel is going to close!  Please save all work before you click OK.")
 
-Remove-Item * -Include *.xlsm, *csv
+Remove-Item * -Include *.xlsm*, *csv
 
 # These registry keys will allow macro security - version 16
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\excel\Security" -Name AccessVBOM -PropertyType DWORD  -Value 1 -Force | Out-Null
@@ -43,7 +44,8 @@ New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\excel\Security" -Na
 Stop-Process -Name "Excel"
 
 # Get all files with xls extension
-Get-ChildItem $PSScriptRoot -Filter *.xls | 
+#Get-ChildItem $PSScriptRoot -Filter *.xls | 
+Get-ChildItem $dir -File | Where-Object { $_.Extension -eq ".xls" } |
 
 # Run this For loop for a files with xls extension
 Foreach-Object {
@@ -66,6 +68,10 @@ $excel = New-Object -ComObject Excel.Application
 # disable visible updating of sheet
 $excel.Visible = $false
 
+# If you want to see what's going on while script runs, uncomment visible and displayalerts below
+#$excel.Visible = $true
+#$excel.DisplayAlerts = $true
+
 $workBook = $excel.Workbooks.Open($excelFile)
 
 # disables the pop up asking if it's ok to overwrite - just overwrite it
@@ -80,10 +86,6 @@ $excel.Quit()
 # Create excel object 
 $excel = New-Object -ComObject Excel.Application
 
-# If you want to see what's going on while script runs, uncomment visible and displayalerts below
-#$excel.Visible = $true
-#$excel.DisplayAlerts = $true
-
 Write-Host "Now processing file: " $excelFile
 $workBook = $excel.Workbooks.Open($excelFile)
 
@@ -92,31 +94,50 @@ $excelModule = $workBook.VBProject.VBComponents.Add(1)
 # This was supposed to load up the entire file as the macro from... the macro but I couldn't get it working
 #$macroImport = [IO.File]::ReadAllText("R:\Nationwide\Nationwide_Export_Prep_Powershell.bas")
 
-# This is the macro
+# This is the VBA Code 
 $excelMacro = @"
-	Sub Nationwide_Export_Prep()
-	Application.ScreenUpdating = False
-    Sheets("report").Select
+'**************************************************************************
 
+    Sub Nationwide_Export_Prep()
+    
+    'Reset the sheet.  Used for debugging and testing
+    'Call Report_Restore
+    
+    Application.ScreenUpdating = False
+    'Application.ScreenUpdating = True
+    Sheets("report").Select
+    
     Rows("1:6").Select
     Selection.Delete Shift:=xlUp
     
-    Columns("J:J").Select
+    'Testing files that already had a g column before it got removed 
+	'Record DEF 2 457 Amount
+	'Call Col_Del_G
+    
+    'Record Roth 457(b) Amount
+    'Create this blank column concatenate columns
+    'Record DEF 2, Record DEF 457 Amount, Record DEFCU 457 Amount
+    Columns("I:I").Select
     Selection.Insert Shift:=xlToRight, CopyOrigin:=xlFormatFromLeftOrAbove
     
-    Range("J2").Select
-    ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-3],RC[-2],RC[-1])"
+    Range("I2").Select
+    'ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-2],RC[-1])"
+    ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-2],RC[-1])"
     ActiveCell.Offset(rowOffset:=1, columnOffset:=0).Activate
     
+   'While ActiveCell.Offset(rowOffset:=0, columnOffset:=-3) <> ""
    While ActiveCell.Offset(rowOffset:=0, columnOffset:=-3) <> ""
-        ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-3],RC[-2],RC[-1])"
+        'ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-3],RC[-2],RC[-1])"
+        ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-2],RC[-1])"
         ActiveCell.Offset(rowOffset:=1, columnOffset:=0).Activate
    Wend
         
-    Columns("M:M").Select
+    'Create this blank column concatenate columns
+    'Record Roth 457(b), Record Roth 457 CU Amount
+    Columns("L:L").Select
     Selection.Insert Shift:=xlToRight, CopyOrigin:=xlFormatFromLeftOrAbove
     
-    Range("M2").Select
+    Range("L2").Select
     ActiveCell.FormulaR1C1 = "=CONCATENATE(RC[-2],RC[-1])"
     ActiveCell.Offset(rowOffset:=1, columnOffset:=0).Activate
     
@@ -125,18 +146,20 @@ $excelMacro = @"
         ActiveCell.Offset(rowOffset:=1, columnOffset:=0).Activate
    Wend
 
-    Columns("K:K").Select
+    'Select Column Record Roth 457(b)
+    Columns("J:J").Select
     Selection.Insert Shift:=xlToRight, CopyOrigin:=xlFormatFromLeftOrAbove
     
-    Columns("J:J").Select
+    Columns("I:I").Select
     Selection.Copy
     
-    Columns("K:K").Select
+    Columns("J:J").Select
     Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
         :=False, Transpose:=False
     Application.CutCopyMode = False
     
-    Columns("G:J").Select
+    'Select Columns Record DEF 2 457 Amount, Record DEF 457 Amount, Record DEFCU 457 Amount
+    Columns("G:I").Select
     Selection.Delete Shift:=xlToLeft
     
     Columns("G:G").Select
@@ -184,29 +207,58 @@ $excelMacro = @"
     ActiveCell.FormulaR1C1 = "Record Roth"
     
     Dim rownumber As Integer
-	Dim rowstring As String
+    Dim rowstring As String
 
 
-	Range("A7").Select
+    Range("A7").Select
 
-	While ActiveCell <> ""
-		rownumber = ActiveCell.Row
+    While ActiveCell <> ""
+        rownumber = ActiveCell.Row
 
-		If ActiveCell <> "" Then
+        If ActiveCell <> "" Then
     
-			ActiveCell.Offset(rowOffset:=1, columnOffset:=0).Activate
-		Else
+            ActiveCell.Offset(rowOffset:=1, columnOffset:=0).Activate
+        Else
            
-		End If
+        End If
 
-	Wend
+    Wend
 
-	rownumber = rownumber + 1
-	rowstring = CStr(rownumber) & ":" & CStr(Rows.Count)
+    rownumber = rownumber + 1
+    rowstring = CStr(rownumber) & ":" & CStr(Rows.Count)
 
-	Rows(rowstring).ClearContents
+    Rows(rowstring).ClearContents
     
 End Sub
+'*********************************************************************************
+
+Sub Report_Restore()
+
+    Application.DisplayAlerts = False
+    Sheets("report").Select
+    ActiveWindow.SelectedSheets.Delete
+    
+    Sheets("report_copy").Select
+    Sheets("report_copy").Name = "report"
+    
+    Sheets("report").Select
+    Sheets("report").Copy After:=Sheets(1)
+    Sheets("report (2)").Select
+    Sheets("report (2)").Name = "report_copy"
+    Sheets("report").Select
+    Range("A1").Select
+    
+End Sub
+'*********************************************************************************
+'*********************************************************************************
+
+Sub Col_Del_G()
+
+' Col_Del Macro
+    Columns("G:G").Select
+    Selection.Delete Shift:=xlToLeft
+End Sub
+'*********************************************************************************
 "@
 
 # This adds the macro to the xlsm file	
@@ -230,4 +282,3 @@ Stop-Process -Name "Excel"
 # These registry keys will disable macro security - version 16
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\excel\Security" -Name AccessVBOM -PropertyType DWORD  -Value 0 -Force | Out-Null
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\excel\Security" -Name VBAWarnings -PropertyType DWORD  -Value 0 -Force | Out-Null
-
